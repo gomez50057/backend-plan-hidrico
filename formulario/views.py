76376bb88ee3fb20c370c18472b7f475e6822a7d
gomez50057 <gomez.50057@gmail.com>
2025-04-24 11:46:21 -0600

    archivos

--- formulario/views.py
+++ formulario/views.py
@@ -6,11 +6,25 @@ from rest_framework.permissions import IsAuthenticated
 class NivelacionTierraViewSet(viewsets.ModelViewSet):
     queryset = NivelacionTierra.objects.all()
     serializer_class = NivelacionTierraSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        # Guarda el usuario autenticado en created_by
-        serializer.save(created_by=self.request.user)
+        # 1) Se crea la instancia sin los archivos (solo datos “no-file”).
+        #    Esto dispara el save(), genera el folio y lo guarda en BD.
+        instance = serializer.save(created_by=self.request.user)
+
+        # 2) Ahora se reasigna los archivos desde request.FILES y se salva de nuevo.
+        #    upload_to() ya verá instance.folio correctamente.
+        archivo = self.request.FILES.get('archivo_pdf')
+        constancia = self.request.FILES.get('constancia_pdf')
+
+        if archivo:
+            instance.archivo_pdf.save(archivo.name, archivo, save=False)
+        if constancia:
+            instance.constancia_pdf.save(constancia.name, constancia, save=False)
+
+        # Se guarda la instancia con los nuevos archivos ya en la ruta correcta.
+        instance.save()
 
 class ArchivoPDFViewSet(viewsets.ModelViewSet):
     queryset = ArchivoPDF.objects.all()
